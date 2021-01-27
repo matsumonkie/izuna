296162f26ba68cdfd0027724729d87784c8580c8
iori <matsuhar@gmail.com>
2021-01-27 11:29:19 +0100

    ghc-8.10.3

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,9 +1,9 @@
 let
-  rev = "2c0f6135aab77ff942b615228882c7dd996e0882";
+  rev = "966489a0312f80e4dd20189f885bc12d6723a9ac";
   extractedTarball = fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = "sha256:00yg8dck4ymcifrxsknnpms52n16xnb8yiqjkby002mbm2aflf45";
+    sha256 = "sha256:10jx7y8bgkqki9nfy6fdg121pixysrrbdlyyy30sv4x65clmalwp";
   };
 in
-  # extractedTarball will be a directory here, and 'import' will automatically append /default.nix here
+# extractedTarball will be a directory here, and 'import' will automatically append /default.nix here
 import extractedTarball
